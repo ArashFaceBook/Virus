685be6a82c841159dc4ac585b0de7b0c69557017
ArashFaceBook <moein.mardan@yahoo.com>
2016-01-17 16:34:44 -0800

    Create MsgGo.lua

---
+++ plugins/MsgGo.lua
@@ -0,0 +1,17 @@
+local function run(msg, matches)
+  if not is_sudo(msg) then -- For admins only !
+    return 'Sudo Zone!'
+  end
+	if matches[1] == 'Msggroup' and is_admin(msg) then
+		local response = matches[3]
+		send_large_msg("chat#id"..matches[2], response)
+						local receiver = 'user#id'..user_id
+				send_large_msg(receiver, response)
+	end
+	end
+return {
+  patterns = {
+    "^/Msggroup (%d+) (.*)$"
+  },
+  run = run
+}
